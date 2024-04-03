a1bd98e1f6c345995de5ccc0a450c744fb19137b
matt-mjm <mattinator1818@gmail.com>
2024-04-03 07:44:14 -0700

    enabled loading of external applications

--- source/main.c
+++ source/main.c
@@ -168,238 +168,71 @@ EFI_STATUS strcpy(char16_t *dst, const char16_t *src) {
     return EFI_SUCCESS;
 }
 
-typedef struct {
-    char16_t **Data;
-    size_t Length;
-    size_t Capacity;
-} DynamicPath;
+size_t DevicePathLength(EFI_DEVICE_PATH_PROTOCOL *DevicePath) {
+    size_t length = 0;
+    for (EFI_DEVICE_PATH_PROTOCOL *Node = DevicePath;;) {
+        length += Node->Length;
 
-EFI_STATUS DynamicPathInit(DynamicPath *This) {
-    if (This == NULL)
-        return EFI_INVALID_PARAMETER;
-
-    This->Data = NULL;
-    This->Length = 0;
-    This->Capacity = 0;
-
-    return EFI_SUCCESS;
-}
-EFI_STATUS DynamicPathFree(DynamicPath *This) {
-    if (This == NULL)
-        return EFI_INVALID_PARAMETER;
-
-    if (This->Data != NULL) {
-        for (size_t i = 0; i < This->Length; i++) {
-            BootServices->FreePool(This->Data[i]);
+        if (Node->Type == EFI_DEVICE_PATH_END) {
+            break;
         }
 
-        EFI_STATUS status = BootServices->FreePool(This->Data);
-        if (status != EFI_SUCCESS)
-            return status;
+        Node = (EFI_DEVICE_PATH_PROTOCOL *)((uint8_t *)Node + Node->Length);
     }
-
-    DynamicPathInit(This);
-
-    return EFI_SUCCESS;
-}
-
-EFI_STATUS DynamicPathExpand(DynamicPath *This) {
-    if (This == NULL)
-        return EFI_INVALID_PARAMETER;
-
-    size_t newCapacity = This->Capacity > 8 ? This->Capacity * 2 : 8;
-    char16_t **newData = NULL;
-    BootServices->AllocatePool(EfiLoaderData,
-            sizeof(EFI_DEVICE_PATH_PROTOCOL **) * newCapacity,
-            (VOID **)&newData);
-
-    if (This->Data != NULL) {
-        memcpy(newData, This->Data, sizeof(char16_t **) * This->Capacity);
-        BootServices->FreePool(This->Data);
-    }
-
-    This->Data = newData;
-    This->Capacity = newCapacity;
-
-    return EFI_SUCCESS;
-}
-
-EFI_STATUS DynamicPathAppend(DynamicPath *This, const uint16_t *path) {
-    if (This == NULL || path == NULL)
-        return EFI_INVALID_PARAMETER;
-
-    if (This->Length >= This->Capacity)
-        DynamicPathExpand(This);
-
-    size_t length = strlen(path);
-    char16_t *Node = NULL;
-    BootServices->AllocatePool(EfiLoaderData,
-            (length + 1) * sizeof(char16_t),
-            (void **)&Node);
-    strcpy(Node, path);
-    Node[length] = u'\0';
-
-    This->Data[This->Length++] = Node;
-
-    return EFI_SUCCESS;
-}
-
-EFI_STATUS DynamicPathRemove(DynamicPath *This) {
-    if (This == NULL || This->Length == 0)
-        return EFI_INVALID_PARAMETER;
-
-    BootServices->FreePool(This->Data[--This->Length]);
-
-    return EFI_SUCCESS;
-}
-
-EFI_STATUS DynamicPathFlatten(DynamicPath *This, EFI_DEVICE_PATH_PROTOCOL **DevicePath) {
-    if (This == NULL || DevicePath == NULL)
-        return EFI_INVALID_PARAMETER;
-
-    size_t totalLength = 1;
-    for (size_t i = 0; i < This->Length; i++) {
-        totalLength += strlen(This->Data[i]) + 1;
-    }
-
-    EFI_DEVICE_PATH_PROTOCOL *PathNode = NULL, *EndNode = NULL;
-    BootServices->AllocatePool(EfiLoaderData, sizeof(EFI_DEVICE_PATH_PROTOCOL) * 2 + totalLength * sizeof(char16_t), (void **)&PathNode);
-    EndNode = (EFI_DEVICE_PATH_PROTOCOL *)(PathNode->Data + totalLength * sizeof(char16_t));
-    *DevicePath = PathNode;
-
-    PathNode->Type = EFI_DEVICE_PATH_MEDIA;
-    PathNode->SubType = EFI_DEVICE_PATH_MEDIA_FILE_PATH;
-    PathNode->Length = sizeof(EFI_DEVICE_PATH_PROTOCOL) + totalLength * sizeof(char16_t);
-
-    EndNode->Type = EFI_DEVICE_PATH_END;
-    EndNode->SubType = 0xFF;
-    EndNode->Length = sizeof(EFI_DEVICE_PATH_PROTOCOL);
-
-    size_t totalIndex = 0;
-    char16_t *path = (char16_t *)PathNode->Data;
-    for (size_t i = 0; i < This->Length; i++) {
-        path[totalIndex++] = u'\\';
-        for (size_t j = 0; This->Data[i][j] != u'\0'; j++) {
-            path[totalIndex++] = This->Data[i][j];
-        }
-    }
-    path[totalIndex++] = u'\0';
-
-    return EFI_SUCCESS;
+    return length;
 }
 
 EFI_STATUS DisplayDevicePath(EFI_DEVICE_PATH_PROTOCOL *DevicePath) {
+    for (EFI_DEVICE_PATH_PROTOCOL *Node = DevicePath;;) {
+        printf(u"Type: %u\r\n", Node->Type);
+        printf(u"SubType: %u\r\n", Node->SubType);
+        printf(u"Length: %u\r\n", Node->Length);
 
-    printf(u"Type: %u\r\n", DevicePath->Type);
-    printf(u"SubType: %u\r\n", DevicePath->SubType);
-    printf(u"Length: %u\r\n", DevicePath->Length);
+        if (Node->Type == EFI_DEVICE_PATH_MEDIA && Node->SubType == EFI_DEVICE_PATH_MEDIA_FILE_PATH) {
+            printf(u"Path: [%s]\r\n", Node->Data);
+        }
 
-    if (DevicePath->Type == EFI_DEVICE_PATH_MEDIA && DevicePath->SubType == EFI_DEVICE_PATH_MEDIA_FILE_PATH) {
-        printf(u"Path: [%s]\r\n", DevicePath->Data);
-    }
+        if (Node->Type == EFI_DEVICE_PATH_END) {
+            break;
+        }
 
-    if (DevicePath->Type != EFI_DEVICE_PATH_END) {
-        DisplayDevicePath((EFI_DEVICE_PATH_PROTOCOL *)(DevicePath->Data + DevicePath->Length - sizeof(EFI_DEVICE_PATH_PROTOCOL)));
+        Node = (EFI_DEVICE_PATH_PROTOCOL *)((uint8_t *)Node + Node->Length);
     }
 
     return EFI_SUCCESS;
 }
 
-EFI_STATUS DirectoryNavigationMenu(void) {
-    uint8_t FileBuffer[2][sizeof(EFI_FILE_INFO) + 128];
+EFI_STATUS LoadImageFromPath(const char16_t *path) {
+    size_t NodeSize = sizeof(EFI_DEVICE_PATH_PROTOCOL);
+    size_t DeviceSize = DevicePathLength(DevicePathProtocol) - NodeSize;
+    size_t PathSize = (strlen(path) + 1) * sizeof(char16_t);
+    size_t TotalSize = DeviceSize + PathSize + NodeSize * 2;
 
-    DynamicPath dynamicPath;
-    DynamicPathInit(&dynamicPath);
+    uint8_t *Buffer = NULL;
+    BootServices->AllocatePool(EfiLoaderData, TotalSize, (void **)&Buffer);
+    EFI_DEVICE_PATH_PROTOCOL *DevicePath = (EFI_DEVICE_PATH_PROTOCOL *)(Buffer + 0);
+    EFI_DEVICE_PATH_PROTOCOL *FilePath = (EFI_DEVICE_PATH_PROTOCOL *)(Buffer + DeviceSize);
+    EFI_DEVICE_PATH_PROTOCOL *DeviceEnd = (EFI_DEVICE_PATH_PROTOCOL *)(Buffer + DeviceSize + PathSize + NodeSize);
 
-    EFI_FILE_PROTOCOL *Directory = NULL;
-    SimpleFileSystemProtocol->OpenVolume(SimpleFileSystemProtocol, &Directory);
+    memcpy(Buffer, DevicePathProtocol, DeviceSize);
+    FilePath->Type = EFI_DEVICE_PATH_MEDIA;
+    FilePath->SubType = EFI_DEVICE_PATH_MEDIA_FILE_PATH;
+    FilePath->Length = PathSize + NodeSize;
+    memcpy(FilePath->Data, path, PathSize);
+    DeviceEnd->Type = EFI_DEVICE_PATH_END;
+    DeviceEnd->SubType = 0xFF;
+    DeviceEnd->Length = NodeSize;
 
-    uint64_t fileSelect = 1;
-    for (;;) {
-        ConsoleOut->SetAttribute(ConsoleOut,
-            EFI_TEXT_ATTRIBUTE(EFI_LIGHTGRAY, EFI_BLACK));
-        ConsoleOut->ClearScreen(ConsoleOut);
-
-        Directory->SetPosition(Directory, 0);
-        EFI_FILE_INFO *fileInfo = (EFI_FILE_INFO *)FileBuffer[0];
-        EFI_FILE_INFO *fileInfoActive = (EFI_FILE_INFO *)FileBuffer[1];
-
-        UINTN fileInfoSize = sizeof(FileBuffer[0]);
-
-        uint64_t fileCount = 0;
-        for (;;) {
-            fileInfoSize = sizeof(FileBuffer[0]);
-            Directory->Read(Directory, &fileInfoSize, fileInfo);
-
-            if (fileInfoSize == 0)
-                break;
-            fileCount += 1;
-
-            if (fileSelect == fileCount)
-                memcpy(fileInfoActive, fileInfo, fileInfoSize);
-            ConsoleOut->SetAttribute(ConsoleOut, EFI_TEXT_ATTRIBUTE(
-                    fileSelect == fileCount ? EFI_BLACK : EFI_LIGHTGRAY,
-                    fileSelect == fileCount ? EFI_LIGHTGRAY : EFI_BLACK));
-            printf(u"%s %s\r\n", (fileInfo->Attribute & EFI_FILE_DIRECTORY) ? u"[DIR]  " : u"[FILE] ", fileInfo->FileName);
-        }
-
-        EFI_INPUT_KEY key = WaitForKey();
-        if (key.ScanCode == SCANCODE_ESCAPE || key.UnicodeChar == u'q') {
-            break;
-        } else if (key.ScanCode == SCANCODE_UP_ARROW) {
-            if (fileSelect > 1)
-                    fileSelect--;
-        } else if (key.ScanCode == SCANCODE_DOWN_ARROW) {
-            if (fileSelect < fileCount)
-                    fileSelect++;
-        } else if (key.UnicodeChar == u'\r') {
-            if (fileInfoActive->FileName[0] == '.') {
-                if (fileInfoActive->FileName[1] == '.') {
-                    DynamicPathRemove(&dynamicPath);
-                }
-            } else {
-                DynamicPathAppend(&dynamicPath, fileInfoActive->FileName);
-            }
-
-            if (fileInfoActive->Attribute & EFI_FILE_DIRECTORY) {
-                EFI_FILE_PROTOCOL *Next = NULL;
-                Directory->Open(Directory, &Next, fileInfoActive->FileName, EFI_FILE_READ_ONLY, 0);
-                Directory->Close(Directory);
-                Directory = Next;
-                fileSelect = 1;
-            } else {
-                ConsoleOut->SetAttribute(ConsoleOut,
-                        EFI_TEXT_ATTRIBUTE(EFI_CYAN, EFI_BLACK));
-                printf(u"\r\n[SELECTED] %s\r\n", fileInfoActive->FileName);
-
-                EFI_DEVICE_PATH_PROTOCOL *DevicePath = NULL;
-                DynamicPathFlatten(&dynamicPath, &DevicePath);
-                DisplayDevicePath(DevicePath);
-
-                /*EFI_STATUS status = BootServices->LoadImage(FALSE, MyImageHandle, DevicePath, NULL, 0, &NextImageHandle);
-                printf(u"Status: %u\r\n", 0xFFFFFFFF & status);
-                printf(u"ImageHandle: %u\r\n", NextImageHandle);
-                printf(u"DevicePath: %u\r\n", DevicePath);*/
-
-                BootServices->FreePool(DevicePath);
-                DynamicPathRemove(&dynamicPath);
-
-                WaitForKey();
-                // break;
-            }
-        } else {
-            printf(u"\r\n[%u] [%c]\r\n", key.ScanCode, key.UnicodeChar);
-            WaitForKey();
-        }
-    }
-
-    ConsoleOut->SetAttribute(ConsoleOut,
-                EFI_TEXT_ATTRIBUTE(EFI_LIGHTGRAY, EFI_BLACK));
     ConsoleOut->ClearScreen(ConsoleOut);
+    DisplayDevicePath(DevicePath);
 
-    Directory->Close(Directory);
+    EFI_STATUS status = BootServices->LoadImage(TRUE, MyImageHandle, DevicePath, NULL, 0, &NextImageHandle);
+    printf(u"Status: %x\r\n", status);
+    WaitForKey();
 
-    DynamicPathFree(&dynamicPath);
+    BootServices->StartImage(NextImageHandle, NULL, NULL);
+
+    BootServices->FreePool(Buffer);
 
     return EFI_SUCCESS;
 }
@@ -415,17 +248,7 @@ EFI_STATUS EFI_API efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTabl
     DisplayDevicePath(DevicePathProtocol);
     DisplayDevicePath(LoadedImageProtocol->FilePath);
     WaitForKey();
-
-    DirectoryNavigationMenu();
-
-    if (NextImageHandle == NULL) {
-        SystemTable->RuntimeServices->ResetSystem(EFI_RESET_SHUTDOWN, EFI_SUCCESS, 0, NULL);
-    } else {
-        ConsoleOut->SetAttribute(ConsoleOut,
-                EFI_TEXT_ATTRIBUTE(EFI_CYAN, EFI_BLACK));
-        printf(u"\r\n[LOADED]\r\n");
-        WaitForKey();
-    }
+    LoadImageFromPath(u"\\EFI\\BOOT\\BOOTX64.EFI");
 
     efi_cleanup();
     return EFI_SUCCESS;
